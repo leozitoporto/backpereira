a1ba2a14731722413627539cb9f7e775dcf07385
Leonardo <leonardo@zungasoft.net>
2021-01-13 20:59:21 -0300

    teste

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'hello world' });
+  return response.json({ message: 'hello world ' });
 });
 
 app.listen(3333, () => {
