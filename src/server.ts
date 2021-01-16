ac7522af956a9d80adacc0805c46e4ec3eacb611
Leonardo <leonardo@zungasoft.net>
2021-01-16 17:21:27 -0300

    eslint, prettier e migrations

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import './shared/infra/typeorm';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'hello world ' });
-});
+app.get('/', (request, response) => response.json({ message: 'hello world ' }));
 
 app.listen(3333, () => {
-  console.log("server started on port 3333");
+  console.log('server started on port 3333');
 });
