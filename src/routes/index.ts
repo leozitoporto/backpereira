6eaed9c7f7e3af753fab4b881734adf64ac5de21
Leonardo <leonardo@zungasoft.net>
2021-01-24 16:49:30 -0300

    Add hash

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import salesRouter from './sales.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/sales', salesRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,5 +1,13 @@
 import { Router } from 'express';
 
-const usersRouter = Router();
+const ordersRouter = Router();
 
-export default usersRouter;
+ordersRouter.post('/', async (request, response) => {
+  try {
+    return response.send();
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default ordersRouter;
