6d24277e68a57a6d55cae2e0f10839d8e1514bee
Leonardo <leonardo@zungasoft.net>
2021-02-19 15:28:25 -0300

    adjustment

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+
 import salesRouter from '@modules/sales/infra/http/routes/sales.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 import ordersRouter from '@modules/orders/infra/http/routes/orders.routes';
 
 const routes = Router();
@@ -11,6 +13,7 @@ routes.use('/sales', salesRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 routes.use('/orders', ordersRouter);
 
 export default routes;
